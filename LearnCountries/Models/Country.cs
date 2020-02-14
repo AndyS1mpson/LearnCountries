d6d3df38fccc3097cea4159ac35db096d0db5bea
AndyS1mpson <werter1555rus@mail.ru>
2020-02-14 19:24:34 +0300

    Create solution and add Country and User models

---
+++ LearnCountries/Models/Country.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LearnCountries.Models
+{
+    public class Country
+    {
+        public string id { get; set; }
+        public string countryName { get; set; }
+        public string capitalName { get; set; }
+        public string flag { get; set; }
+    }
+}
---
+++ LearnCountries/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LearnCountries.Models
+{
+    public class User
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+        public int score { get; set; }
+    }
+}
